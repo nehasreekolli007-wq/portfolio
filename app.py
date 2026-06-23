838f488aa94a52216bec09185811f6593e45150e
Nehas <nehas@example.com>
2026-06-23 22:59:40 +0530

    Added deployment configuration

--- app.py
+++ app.py
@@ -53,5 +53,8 @@ def delete_project(id):
 
     return redirect('/admin')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
